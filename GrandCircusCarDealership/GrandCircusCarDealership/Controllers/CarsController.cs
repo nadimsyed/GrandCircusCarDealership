cfea42207cefdd39128192d4083ffdc2304d2393
Nadim <syed.m.nadim@gmail.com>
2018-06-13 15:19:26 -0400

    Did all 4 searches, and the search feature for multi-search

---
+++ GrandCircusCarDealership/GrandCircusCarDealership/Controllers/CarsController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GrandCircusCarDealership.Models;
+
+namespace GrandCircusCarDealership.Controllers
+{
+    public class CarsController : ApiController
+    {
+        private CarDealershipEntities db = new CarDealershipEntities();
+
+        // GET: api/Cars
+        public IQueryable<Car> GetCars()
+        {
+            return db.Cars;
+        }
+
+        // GET: api/Cars/5
+        [ResponseType(typeof(Car))]
+        public IHttpActionResult GetCar(int id)
+        {
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(car);
+        }
+
+        [ResponseType(typeof(Car))]
+        public IHttpActionResult GetCarByMake(string make)
+        {
+            List<Car> list = db.Cars.Where(
+               c => c.Make.Contains(make)
+                ).ToList();
+
+            return Ok(list);
+        }
+
+        [ResponseType(typeof(Car))]
+        public IHttpActionResult GetCarByModel(string model)
+        {
+            List<Car> list = db.Cars.Where(
+               c => c.Model.Contains(model)
+                ).ToList();
+
+            return Ok(list);
+        }
+
+        [ResponseType(typeof(Car))]
+        public IHttpActionResult GetCarByYear(int year)
+        {
+            List<Car> list = db.Cars.Where(
+               c => c.Year == year
+                ).ToList();
+
+            return Ok(list);
+        }
+
+        [ResponseType(typeof(Car))]
+        public IHttpActionResult GetCarByColor(string color)
+        {
+            List<Car> list = db.Cars.Where(
+               c => c.Color.Contains(color)
+                ).ToList();
+
+            return Ok(list);
+        }
+
+        [ResponseType(typeof(Car))]
+        public IHttpActionResult GetCarByEverything(string make = "boo", string model = "boo", int year = 0, string color = "boo")
+        {
+            List<Car> cars = (from c in db.Cars
+                              select c).ToList();
+            if (make != "" && make != "boo" && make != null)
+            {
+                cars = (from c in cars
+                        where c.Make.Contains(make)
+                        select c
+                        ).ToList();
+            }
+            if(model != "" && model != "boo" && model != null)
+            {
+                cars = (from c in cars
+                        where c.Model.Contains(model)
+                        select c).ToList();
+            }
+            if (year != 0)
+            {
+                cars = (from c in cars
+                        where c.Year == year
+                        select c).ToList();
+            }
+            if (color != "" && color != "boo" && color != null)
+            {
+                cars = (from c in cars
+                        where c.Color.Contains(color)
+                        select c).ToList();
+            }
+
+            return Ok(cars);
+        }
+
+        //// PUT: api/Cars/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutCar(int id, Car car)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != car.CarId)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(car).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!CarExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //// POST: api/Cars
+        //[ResponseType(typeof(Car))]
+        //public IHttpActionResult PostCar(Car car)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Cars.Add(car);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = car.CarId }, car);
+        //}
+
+        //// DELETE: api/Cars/5
+        //[ResponseType(typeof(Car))]
+        //public IHttpActionResult DeleteCar(int id)
+        //{
+        //    Car car = db.Cars.Find(id);
+        //    if (car == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Cars.Remove(car);
+        //    db.SaveChanges();
+
+        //    return Ok(car);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CarExists(int id)
+        {
+            return db.Cars.Count(e => e.CarId == id) > 0;
+        }
+    }
+}
---
+++ GrandCircusCarDealership/GrandCircusCarDealership/Controllers/HomeController.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GrandCircusCarDealership.Models;
+using Newtonsoft.Json.Linq;
+
+namespace GrandCircusCarDealership.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetByCarID(int CarID)
+        {
+            ViewBag.CarID = CarID;
+            return View();
+        }
+
+        //[Route("~/api/cars/GetCarByMake/{make:string}/cars")]
+        //public ActionResult GetCarsByMake(string publisher)
+        //{
+        //    //LibraryEntities db = new LibraryEntities();
+
+        //    List<Car> list = db.Car.Where(
+        //       c => c.Publisher.Contains(publisher)
+        //        ).ToList();
+
+        //    return Json(list);
+        //}
+
+        public ActionResult GetByCarMake(string make)
+        {
+            HttpWebRequest WR = WebRequest.CreateHttp($"http://localhost:52043/api/Cars/GetCarByMake?make={make}");
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)WR.GetResponse();
+            }
+            catch (WebException e)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string Data = reader.ReadToEnd();
+
+            try
+            {
+                JArray JsonData = JArray.Parse(Data);
+                ViewBag.Cars = JsonData/*["Make"]*/;
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+        }
+
+        public ActionResult GetByCarModel(string model)
+        {
+            HttpWebRequest WR = WebRequest.CreateHttp($"http://localhost:52043/api/Cars/GetCarByModel?model={model}");
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)WR.GetResponse();
+            }
+            catch (WebException e)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string Data = reader.ReadToEnd();
+
+            try
+            {
+                JArray JsonData = JArray.Parse(Data);
+                ViewBag.Cars = JsonData/*["Make"]*/;
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+        }
+
+        public ActionResult GetByCarYear(int year)
+        {
+            HttpWebRequest WR = WebRequest.CreateHttp($"http://localhost:52043/api/Cars/GetCarByYear?year={year}");
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)WR.GetResponse();
+            }
+            catch (WebException e)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string Data = reader.ReadToEnd();
+
+            try
+            {
+                JArray JsonData = JArray.Parse(Data);
+                ViewBag.Cars = JsonData/*["Make"]*/;
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+        }
+
+        public ActionResult GetByCarColor(string color)
+        {
+            HttpWebRequest WR = WebRequest.CreateHttp($"http://localhost:52043/api/Cars/GetCarByColor?color={color}");
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)WR.GetResponse();
+            }
+            catch (WebException e)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string Data = reader.ReadToEnd();
+
+            try
+            {
+                JArray JsonData = JArray.Parse(Data);
+                ViewBag.Cars = JsonData/*["Make"]*/;
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+        }
+
+        public ActionResult GetEverythingStart()
+        {
+
+            return View();
+        }
+
+        public ActionResult GetByCarEverything(string make = "boo", string model = "boo", int year = 0, string color = "boo")
+        {
+            HttpWebRequest WR = WebRequest.CreateHttp($"http://localhost:52043/api/Cars/GetCarByEverything?make={make}&model={model}&year={year}&color={color}");
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)WR.GetResponse();
+            }
+            catch (WebException e)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string Data = reader.ReadToEnd();
+
+            try
+            {
+                JArray JsonData = JArray.Parse(Data);
+                ViewBag.Cars = JsonData/*["Make"]*/;
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+        }
+    }
+}
